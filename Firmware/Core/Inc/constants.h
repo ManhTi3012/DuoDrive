a3868f749bce753edb84042a563f16a582fd9100
ManhTi <87491627+ManhTi3012@users.noreply.github.com>
2025-04-30 17:22:52 +0700

    Update constants.h

--- Firmware/Core/Inc/constants.h
+++ Firmware/Core/Inc/constants.h
@@ -5,10 +5,35 @@
  *      Author: Ti Manh
  */
 
-#ifndef INC_CONSTANTS_H_
-#define INC_CONSTANTS_H_
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 
 
+#define MAX_DUTY 999
+
+#define MIN_DUTY 0
+
+#define ADC_RESOLUTION_ 12
+
+#define PI 3.14159265359f
+
+#define PI2 6.283185307f
+
+#define SQRT3DEV2 0.8660254038f
+
+#define CPR 16384
+
+#define RAD_CONST 0.000383495187f
+
+#define CPR2 16384 / 2
+
+#define PWM_FREQ 20000
+
+#define LOOP_FREQ 5000
+
+#define LOOP_TIME 0.0002
+
+#define SERIAL_SPEED 115200
 
 
 
